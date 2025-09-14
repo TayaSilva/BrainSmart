aeb0d864e4ad31097b0e029567b7ff3218704977
TaianeSilva99 <taianens1d@gmail.com>
2025-09-14 11:22:26 -0300

    feat: adicionado a pagina de registro

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,68 +1,72 @@
 'use client';
 import Image from 'next/image';
-import BackLogin from '@/../public/images/background-login.png';
-import BgLogin from '@/../public/images/bg-login.svg';
+import BackRegister from '@/../public/images/background-register.png';
+import BgRegister from '@/../public/images/bg-register.jpg';
 import GoogleLogo from '@/../public/images/google-logo.png';
 import Input from '@/app/components/Input/Input';
 import Link from 'next/link';
 import GradientButton from '@/app/components/GradientButton/GradientButton';
 import { signIn } from 'next-auth/react';
 
-export default function Login() {
+export default function Register() {
 	return (
 		<div
 			className="relative min-h-screen flex items-center justify-center overflow-hidden "
 			style={{
-				backgroundImage: `url(${BgLogin.src})`,
+				backgroundImage: `url(${BgRegister.src})`,
 				backgroundSize: 'cover',
 				backgroundPosition: 'center',
 			}}
 		>
 			<div className="flex flex-col justify-center w-full md:flex-row z-10 ">
 				<div className="relative z-10 w-full max-w-md bg-white/10 backdrop-blur-md border border-white/30 rounded-tl-2xl rounded-bl-2xl shadow-2xl p-8 flex flex-col items-center justify-center">
-					<h1 className="text-2xl font-bold uppercase">login</h1>
-					<button
+					<h1 className="text-2xl font-bold uppercase">cadastre-se</h1>
+					{/* <button
 						onClick={() => signIn('google')}
 						className="my-4 cursor-pointer w-full flex items-center justify-center gap-2 bg-white/80 text-gray-800 font-semibold py-2 px-4 rounded-lg shadow hover:bg-gray-100 transition"
 					>
 						<Image src={GoogleLogo} alt="Google Logo" width={20} height={20} />
 						Faça login com Google
-					</button>
+					</button> */}
 					<div className="flex w-full gap-4 flex-col">
-						<Input label="E-mail" type="email" placeholder="example@email.com" />
+						<Input
+							label="Nome"
+							type="text"
+							placeholder="Digite seu nome completo"
+							minLength={10}
+						/>
+						<Input label="Apelido" type="text" placeholder="Como quer ser chamado?" />
+						<Input label="Email" type="e-mail" placeholder="Digite seu email" />
+						<Input
+							label="CPF"
+							type="text"
+							placeholder="Digite o seu CPF"
+							mask="###.###.###-##"
+						/>
 						<Input label="Senha" type="password" placeholder="Digite sua senha" />
 					</div>
-					<GradientButton className="my-6">Entrar</GradientButton>
-					<div className="mt-4 flex flex-row text-sm gap-1">
-						<p>Puts, esqueceu a senha? </p>
-						<Link
-							href="/"
-							className="hover:text-text-primary underline hover:font-semibold"
-						>
-							Troque aqui
-						</Link>
-					</div>
+					<GradientButton className="mt-6">Cadastre-se</GradientButton>
 				</div>
 
-				<div className="relative z-10 flex-col w-full max-w-md bg-[#EA4C89] border-white/30 border rounded-br-2xl rounded-tr-2xl shadow-2xl p-8 flex items-center justify-center">
+				<div className="relative z-10 flex-col w-full max-w-md bg-[#8d5cf6] border-white/30 border rounded-br-2xl rounded-tr-2xl shadow-2xl p-8 flex items-center justify-center">
 					<Image
-						src={BackLogin}
+						src={BackRegister}
 						alt="Background Login"
 						className="w-[200px]h-[200px]"
 					/>
 					<div className="mt-4  flex flex-row text-sm gap-1">
-						<p>Você AINDA não tem conta? </p>
+						<p>Já tem conta? </p>
 						<Link
-							href="/register"
+							href="/login"
 							className="hover:text-primary underline hover:font-semibold"
 						>
-							Registre-se
+							Faça login
 						</Link>
 					</div>
 				</div>
 			</div>
 
-			<div className="absolute top-4 right-4 w-fit text-white/90 flex flex-col items-center">
+			<div className="absolute bottom-4 right-4 w-fit text-white/90 flex flex-col items-center">
 				<h2 className="text-2xl font-bold">BrainSmart</h2>
 				<p className="text-sm text-center">Sua mente no comando da tecnologia</p>
 			</div>
--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	label: string;
+	minLength?: number;
+	maxLength?: number;
+	mask?: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, type = 'text', ...rest }) => {
+const Input: React.FC<InputProps> = ({
+	label,
+	type = 'text',
+	minLength,
+	maxLength,
+	mask,
+	...rest
+}) => {
+	const [value, setValue] = useState('');
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		let val = e.target.value;
+
+		if (type === 'number') {
+			val = val.replace(/\D/g, '');
+		} else if (type === 'text') {
+			val = val.replace(/[^a-zA-Z\s]/g, '');
+		}
+
+		if (mask) {
+			let maskedValue = '';
+			let i = 0;
+			for (const m of mask) {
+				if (m === '#') {
+					if (val[i]) maskedValue += val[i++];
+				} else {
+					maskedValue += m;
+				}
+			}
+			val = maskedValue;
+		}
+
+		if (maxLength) {
+			val = val.slice(0, maxLength);
+		}
+
+		setValue(val);
+	};
+
 	return (
 		<div className="flex flex-col w-full">
 			<label className="mb-1 text-white font-medium text-sm">{label}</label>
 			<input
-				type={type}
+				type="text"
+				value={value}
+				minLength={minLength}
+				maxLength={maxLength}
+				onChange={handleChange}
 				{...rest}
 				className="
           w-full
@@ -28,7 +73,7 @@ const Input: React.FC<InputProps> = ({ label, type = 'text', ...rest }) => {
           placeholder-gray-400
           transition
           duration-200
-          "
+        "
 			/>
 		</div>
 	);
